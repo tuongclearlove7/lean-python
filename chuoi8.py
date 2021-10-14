6b48d8db090f8df9c777aa6e9bc90251d2d78563
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2021-10-14 20:08:19 +0700

    Update chuoi8.py

--- chuoi8.py
+++ chuoi8.py
@@ -1,49 +1,49 @@
-a = 'how kteam free education'
+a = 'Tuong Clearlove7'
 b = a.split(' ', 2)
 # Phương thức split cắt khoảng
 print(a)
 print(b)
 
-a = 'how kteam free education'
+a = 'Tuong Clearlove7'
 b = a.partition('e')
 # Phương thức partition cắt chuỗi 
 print(a)
 print(b)
 
-a = 'how kteam free education'
+a = 'Tuong Clearlove7'
 b = a.count('e',0,10)
 # Phương thức count tìm từ vị trí con trong chuỗi trong khoảng từ 0 đến 10
 print(a)
 print(b)
 
-a = 'how kteam free education'
-b = a.startswith('k',4,5)
+a = 'Tuong Clearlove7'
+b = a.startswith('7',4,5)
 # Phương thức  startstwith (True or false)
 print(a)
 print(b)
 
-a = 'how kteam free education'
-b = a.find('e')
+a = 'Tuong Clearlove7'
+b = a.find('Tuong')
 # Phương thức  find tìm là số thứ tự của chữ trog chuỗi
 # rfind là từ thứ tự từ phải qua 
 print(a)
 print(b)
 
-a = 'how kteam free education'
-b = a.rindex('k')
+a = 'Tuong Clearlove7'
+b = a.rindex('7')
 # Phương thức  index giống find
 #  là từ thứ tự từ phải qua 
 print(a)
 print(b)
 
-a = 'how kteam free education'
-b = a.index('k')
+a = 'Tuong Clearlove7'
+b = a.index('7')
 # Phương thức  find tìm là số thứ tự của chữ trog chuỗi
 # khác find ở cái là tìm ra nếu k thấy thì sẽ lỗi
 print(a)
 print(b)
 
-a = 'how kteam free education'
+a = 'Tuong Clearlove7'
 b = a.islower()
 # Phương thức  islower (true of false)
 # tuong tự là phương thức isupper
