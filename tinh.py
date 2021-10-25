b70ed063137b152acf875dc02677e7acb1117d54
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2021-10-25 17:00:16 +0700

    Add files via upload

---
+++ tinh.py
@@ -0,0 +1,16 @@
+print('bài toán tính tổng 2 số')
+so1 = int(input('nhập số thứ nhất : '))
+so2 = int(input('nhập số thứ 2 : '))
+print('kết quả là : ',so1 + so2)
+
+
+print('bài toán tính tích 2 số')
+so1 = int(input('nhập số thứ nhất : '))
+so2 = int(input('nhập số thứ 2 : '))
+print('kết quả là : ',so1 * so2)
+
+
+print('bài toán tính Thương 2 số')
+so1 = int(input('nhập số thứ nhất : '))
+so2 = int(input('nhập số thứ 2 : '))
+print('kết quả là : ',so1 / so2)
