de90d0395f415b1809c8ba2d87636109622f999f
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2021-10-31 14:49:38 +0700

    Update selenium_gmail.py

--- selenium_gmail.py
+++ selenium_gmail.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 tuong = webdriver.Chrome(executable_path=r"C:\Users\DELL\Documents\selenium\chromedriver.exe")
 tuong.set_window_size(1000,1000)
 tuong.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
-tuong.find_element_by_id("identifierId").send_keys("lol00sever@gmail.com")
+tuong.find_element_by_id("identifierId").send_keys("")
 sleep(2)
 tuong.find_element_by_id("identifierNext").click()
 
