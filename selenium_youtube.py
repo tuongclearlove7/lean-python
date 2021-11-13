eccd5c16390902bdb934ee613cd8f53ad1186fba
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2021-11-13 22:56:32 +0700

    Update selenium_youtube.py

--- selenium_youtube.py
+++ selenium_youtube.py
@@ -6,7 +6,7 @@ from colorama import init, AnsiToWin32
 import pyttsx3
 bot = pyttsx3.init()  
 voices = bot.getProperty('voices') #nhận thông tin về giọng
-bot.setProperty('voice', voices[1].id)  # giọng nữ
+bot.setProperty('voice', voices[1].id)  # giọng nữ id là 1
 init(wrap=False)
 stream = AnsiToWin32(sys.stderr).stream
 turn_on = input(max(["""
@@ -45,15 +45,15 @@ start_on("""
                           └─────────────┘""")
 sleep(1)
 def end_bot(exit="""          
-                              ┌─────┐
-                              │ Tắt │
-                              └─────┘"""):
+                                   ┌─────┐
+                                   │ Tắt │
+                                   └─────┘"""):
     print(exit)
 print(Fore.BLUE + "", file=stream)
 def start_git(on_github="""
-                         ┌────────────┐
-                         │ Bật Github │
-                         └────────────┘"""):
+                              ┌────────────┐
+                              │ Bật Github │
+                              └────────────┘"""):
     print(on_github)
 def login_git(login):
     print(login)
@@ -64,9 +64,9 @@ sleep(3)
 bot.say("press enter to control over me")
 bot.runAndWait()
 input("""\n 
-           ┌───────────────────────────────────────┐
-           │ông chủ bấm enter ra lệnh cho cháu đi ạ│
-           └───────────────────────────────────────┘\n""")
+               ┌───────────────────────────────────────┐
+               │ông chủ bấm enter ra lệnh cho cháu đi ạ│
+               └───────────────────────────────────────┘\n""")
 bot.say("turn on Facebook")
 bot.runAndWait()
 print("""\n
@@ -79,9 +79,9 @@ bot.say("press enter to control over me")
 bot.runAndWait()
 print(Fore.WHITE + "", file=stream)
 input('''\n
-           ┌───────────────────────────────────────┐
-           │ông chủ bấm enter ra lệnh cho cháu đi ạ│
-           └───────────────────────────────────────┘\n''')
+                 ┌───────────────────────────────────────┐
+                 │ông chủ bấm enter ra lệnh cho cháu đi ạ│
+                 └───────────────────────────────────────┘\n''')
 start_on("""\n
                            ┌───────────────┐
                            │Bật google dịch│
@@ -89,14 +89,17 @@ start_on("""\n
 bot.say("turn on google translate")
 bot.runAndWait()
 tuong.get("https://www.google.com/search?q=gg+d%E1%BB%8Bch&oq=gg&aqs=chrome.0.69i59j69i57j0i131i433i512l2j0i433i512j0i131i433i512l2j69i60.2320j0j7&sourceid=chrome&ie=UTF-8")
+tuong.find_element_by_id("tw-source-text-ta").send_keys("""con bot của cháu tên là : Warker_YTt sophia :D .
+Nó hơi chậm vì cháu để nhiều sleep time mong mọi ng thông cảm. :D
+""")
 sleep(1)
 bot.say("press enter to control over me")
 bot.runAndWait()
 print(Fore.MAGENTA + "", file=stream)
 input("""\n
-           ┌───────────────────────────────────────┐
-           │ông chủ bấm enter ra lệnh cho cháu đi ạ│
-           └───────────────────────────────────────┘\n""")
+               ┌───────────────────────────────────────┐
+               │ông chủ bấm enter ra lệnh cho cháu đi ạ│
+               └───────────────────────────────────────┘\n""")
 bot.say("turn on github")
 bot.runAndWait()
 start_git()
@@ -107,25 +110,25 @@ tuong.get("https://github.com/login")
 bot.say("login")
 bot.runAndWait()
 login_git("""\n
-                           ┌─────────┐
-                           │Đăng nhập│
-                           └─────────┘\n""")
+                                ┌─────────┐
+                                │Đăng nhập│
+                                └─────────┘\n""")
 sleep(1)
 tuong.find_element_by_id("login_field").send_keys("")
 sleep(1)
 login_git("""\n
-                         ┌──────────────┐
-                         │Nhập mật khẩu │
-                         └──────────────┘\n""")
+                              ┌──────────────┐
+                              │Nhập mật khẩu │
+                              └──────────────┘\n""")
 sleep(1)
 bot.say("enter password")
 bot.runAndWait()
 tuong.find_element_by_id("password").send_keys("")
 sleep(1)
 start_git("""\n
-                          ┌─────────────┐
-                          │Đăng nhập vào│
-                          └─────────────┘\n""")
+                               ┌─────────────┐
+                               │Đăng nhập vào│
+                               └─────────────┘\n""")
 bot.say("click sign in")
 bot.runAndWait()
 tuong.find_element_by_name("commit").click()
@@ -135,21 +138,21 @@ bot.say("press enter to control over me")
 bot.runAndWait()
 print(Fore.GREEN + "", file=stream)
 input("""\n 
-            ┌───────────────────────────────────────┐
-            │ông chủ bấm enter ra lệnh cho cháu đi ạ│
-            └───────────────────────────────────────┘\n""")
+                   ┌───────────────────────────────────────┐
+                   │ông chủ bấm enter ra lệnh cho cháu đi ạ│
+                   └───────────────────────────────────────┘\n""")
 end_bot()
 print(Fore.RED + "", file=stream)
 bot.say("exit bot")
 bot.runAndWait()
 bot_turn_off = '{:^55}'.format("""\n
-            ┌───────────────────────────────────────┐
-            │Vâng ạ, cháu tắt đây ạ tạm biệt ông chủ│
-            └───────────────────────────────────────┘\n""")
+                   ┌───────────────────────────────────────┐
+                   │Vâng ạ, cháu tắt đây ạ tạm biệt ông chủ│
+                   └───────────────────────────────────────┘\n""")
 for i in bot_turn_off:
     print(i,end="", flush=True)
     sleep(0.01)
-bot.say("Yes, I will turn it off bye bye boss")
+bot.say("Yes, I will turn off , good bye boss")
 bot.runAndWait()
 sleep(1.5)
 tuong.quit()
