dac1e86c63dcc14ff1eafbef04485f55d62802fe
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2021-12-02 16:12:31 +0700

    Update tkinter_test.py

--- tkinter_test.py
+++ tkinter_test.py
@@ -15,14 +15,12 @@ from time import strftime
 from threading import *
 import pygame
 
-#class main():
 init(wrap=False)
 stream = AnsiToWin32(sys.stderr).stream # text color
 pygame.init()
 bot = pyttsx3.init()
 voices = bot.getProperty('voices') # information about voices (thông tin về giọng nói)
 bot.setProperty('voice', voices[1].id) #female voice id 1 (giọng nữ id là 1)
-
 tool = Tk()
 tool.geometry("560x220")# set screen (thiết lập khung tool)
 # this will create a label widget (tạo tiện ích label : nhãn)
@@ -31,7 +29,6 @@ tool.title("My Software")# set title
 tool.configure(background="#099D9D") # set background
 # grid method to arrange labels in respective (phương pháp lưới để sắp xếp các nhãn)
 # rows and columns as specified  (tương ứng hàng và cột như đã chỉ định)
-
 class Class_multithreading: # class multithreading (lớp đa luồng)
     def __init__(self):
             super().__init__()
@@ -50,10 +47,8 @@ class Class_multithreading: # class multithreading (lớp đa luồng)
         self.multithreading4.start()
     def multithreading_sound(self):
         self.multithreading_S=Thread(target=play_sound)
-        self.multithreading_S.start() #multi()
+        self.multithreading_S.start()
 object = Class_multithreading()
-print(type(object))
-
 #setting sound and time in tool (thiết lập thời gian trong tool) 
 def multithreading_game():
     multithreading_game7=Thread(target=play_game)
@@ -62,18 +57,16 @@ def play_game():
     import game7
     pass
 def play_sound():
-    pygame.mixer.music.load("tuong.mp3") #Loading File Into Mixer
+    pygame.mixer.music.load("vippro73.mp3") #Loading File Into Mixer
     pygame.mixer.music.play() #Playing It In The Whole Device
 def new_time():
     time_string = strftime("Time : %H:%M:%S %p") # time format (định dạng thời gian)
-    #print(time_string)
     label_time.config(text = time_string)
     label_time.after(1000, new_time) # time delay of 1000 milliseconds (thời gian dừng khoảng 1000 mili giây)
 def on_youtube():
     bot.say("turn on youtube")# bot said 
     if bot.runAndWait() == print(Fore.RED + "", file=stream):# print text color (in chữ màu)
         tuong = webdriver.Chrome(executable_path=r"C:\Users\DELL\Documents\selenium\chromedriver.exe")
-        #print("tool running please wait !\n"*1000)
         tuong.set_window_size(750,700) == tuong.get("https://www.youtube.com/watch?v=sX1Y2JMK6g8")
 def on_facebook():
     bot.say("turn on facebook")
@@ -102,9 +95,8 @@ def on_github():
         tuong.execute_script("window.scroll(0,900)")
     if sleep(3)== print("coming profile"):
         tuong.get("https://github.com/tuongclearlove7")
-
-    #setting time and my name (thiết lập thời gian và tên của tôi)
-    # I will become a Software Developer (tôi sẽ trở thành một nhà phát triển phần mềm trong tương lai)
+#setting time and my name (thiết lập thời gian và tên của tôi)
+# I will become a Software Developer (tôi sẽ trở thành một nhà phát triển phần mềm trong tương lai)
 my_font=('fantasy',10,'bold')
 label_time=tk.Label(tool,font=my_font, bg="#099D9D",fg="black")
 label_time.grid(row=0,column=0,padx=5,pady=25)
@@ -115,10 +107,8 @@ l1 = Label(tool, text = name_dev, font = font_dev, fg = color_dev, bg="#099D9D")
 l1.grid(row = 1, column = 0, sticky = W, pady = 1)
 # grid method to arrange labels in respective ( phương pháp lưới để sắp xếp các nhãn tương ứng)
 # rows and columns as specified (hàng và cột như đã chỉ định)
-
 tick = Checkbutton(tool, text = "Accept", bg="#099D9D",width=5)# set tick (thiết lập ô tick)
 tick.grid(row = 2, column = 0, sticky = W, columnspan = 2)#position tick and pressed the tick (vị trí tick và bấm vào tick)
-
 # adding image (remember image should be PNG and not JPG) (thêm ảnh hãy nhớ hình ảnh phải là PNG chứ không phải JPG )
 img = PhotoImage(file = "clearlove7.png")# set image (thiết lập ảnh)
 img1 = img.subsample(2, 3)
@@ -135,19 +125,17 @@ b2 = Button(tool, text= "Facebook",bg="blue",activebackground=click_bg_button_F,
 b3 = Button(tool, text = "Google translate",bg="white",activebackground=click_bg_button_GG, width = 13, command=object.multithreading_google_translate)
 b4 = Button(tool, text = "Github", bg="#663399",activebackground=click_bg_button_Git ,width = 7, command=object.multithreading_github)
 button_sound = Button(tool,text="introduce",bg="#006699",activebackground="#99CCFF",command=object.multithreading_sound)
-button_game = Button(tool,text="play game",bg="#FF6600",activebackground="#99CCFF",width = 8 ,command=multithreading_game)
-
+button_game = Button(tool,text="play game",bg="#3366CC",activebackground="#99CCFF",width = 8 ,command=multithreading_game)
 b1.grid(row = 2, column = 1)#potision button(vị trí nút)
 b2.grid(row = 2, column = 2)
 b3.grid(row = 2, column = 3)
 b4.grid(row = 2, column = 4)
 button_sound.grid(row=1, column=1)
-button_game.grid(row=1,column=1, sticky=S)
+button_game.grid(row=1,column=4)
  # infinite loop which can be terminated (mainloop) (vòng lặp vô hạn có thể được kết thúc bởi mainloop())
  # the tool will by keyboard or mouse interrupt(tool sẽ bị ngắt bằng bàn phím điều khiển hoặc chuột)
 new_time()
 tool.mainloop()
 #import hack_log
-sys.exit("end tool")
-
+sys.exit("end tool")  
 
